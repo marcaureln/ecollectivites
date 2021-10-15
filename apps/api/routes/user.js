4a9a5b95c1d1c4ae7fca63472a2109c6f5ecdd74
Alex N'Guessan <alexmarcaureln@gmail.com>
2021-10-15 13:19:16 +0000

    Rename /api/auth/ to /api/auth/verify

--- apps/api/routes/user.js
+++ apps/api/routes/user.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const userController = require('../controllers/user');
 
-router.post('/', userController.verify);
+router.post('/verify', userController.verify);
 router.post('/check', userController.check);
 router.post('/signup', userController.signup);
 router.post('/login', userController.login);
