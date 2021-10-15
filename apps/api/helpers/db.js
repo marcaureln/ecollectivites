5502b2e06121476afe71032ba08bd2a9757da5e8
Alex N'Guessan <alexmarcaureln@gmail.com>
2021-10-15 08:43:38 +0000

    Setup database helper

---
+++ apps/api/helpers/db.js
@@ -0,0 +1,15 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+	host: process.env.PGHOST,
+	port: process.env.PGPORT,
+	user: process.env.PGUSER,
+	password: process.env.PGPASSWORD,
+	database: process.env.PGDATABASE,
+});
+
+module.exports = {
+	query: (queryString, params, callback) => {
+		return pool.query(queryString, params, callback);
+	},
+};
