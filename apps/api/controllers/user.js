a75584f19c8d5d9b799dc99f853f47573eabc5b9
Alex N'Guessan <alexmarcaureln@gmail.com>
2021-10-22 10:00:25 +0000

    Make user controller handle requests safer

--- apps/api/controllers/user.js
+++ apps/api/controllers/user.js
@@ -12,17 +12,15 @@ const db = require('../helpers/db');
 exports.verify = function (req, res, next) {
 	const phone = req.body.phone;
 
-	if (!phone || phone.length > 10) {
-		res.status(400).json({ error: 'Invalid phone number' });
-		return;
+	if (!phone || phone.length != 10) {
+		return res.status(400).json({ error: 'Invalid phone number' });
 	}
 
 	twilio.verify
 		.services(verifyServiceSid)
 		.verifications.create({ to: '+225' + phone, channel: 'sms', locale: 'fr' })
 		.then((verification) => {
-			let message = 'Code sended to ' + verification.to + ' verification ' + verification.status;
-			res.status(200).json({ message });
+			res.status(200).json({ message: 'Verification code sended to ' + verification.to });
 		})
 		.catch((error) => {
 			res.status(error.status).json({ error: error.message });
@@ -33,28 +31,22 @@ exports.check = function (req, res, next) {
 	const phone = req.body.phone;
 	const code = req.body.code;
 
-	if (!phone || !code || phone.length > 10) {
-		res.status(400).json({ error: 'Invalid code or phone number' });
-		return;
+	if (!phone || !code || phone.length != 10) {
+		return res.status(400).json({ error: 'Invalid code or phone number' });
 	}
 
 	twilio.verify
 		.services(verifyServiceSid)
 		.verificationChecks.create({ to: '+225' + phone, code: code })
 		.then((verificationCheck) => {
-			switch (verificationCheck.status) {
-				case 'approved':
-					const token = jwt.sign({ phone }, jwtSecret, { algorithm: 'HS256', expiresIn: '10m' });
-					res.status(200).json({ phone, token });
-					break;
-				case 'canceled':
-					res.status(404).json({ message: 'Verification cancelled!' });
-					break;
-				default:
-					res.status(401).json({ message: 'Wrong code!' });
-					break;
+			if (verificationCheck.status == 'approved') {
+				const token = jwt.sign({ phone }, jwtSecret, { expiresIn: '10m' });
+				res.status(200).json({ phone, token });
+			} else if (verificationCheck.status == 'canceled') {
+				res.status(404).json({ message: 'Verification cancelled!' });
+			} else {
+				res.status(401).json({ message: 'Wrong code!' });
 			}
-			return;
 		})
 		.catch((error) => {
 			res.status(error.status).json({ error: error.message });
@@ -62,45 +54,70 @@ exports.check = function (req, res, next) {
 };
 
 exports.signup = async function (req, res, next) {
-	const token = req.headers.authorization.split(' ')[1];
-	const decodedToken = jwt.decode(token, jwtSecret);
+	const decodedToken = (function () {
+		try {
+			const token = req.headers.authorization.split(' ')[1];
+			return jwt.decode(token, jwtSecret);
+		} catch {
+			return undefined;
+		}
+	})();
 
-	const firstname = req.body.firstname;
-	const lastname = req.body.lastname;
-	const phone = req.body.phone;
+	if (!decodedToken) {
+		return res.status(401).json({ error: 'Invalid token' });
+	}
 
-	if (firstname && lastname && phone && phone == decodedToken.phone) {
-		db.query('INSERT INTO users (firstname, lastname, phone) VALUES ($1, $2, $3)', [firstname, lastname, phone])
-			.then((result) => {
-				const token = jwt.sign({ firstname, lastname, phone }, jwtSecret, { algorithm: 'HS256', expiresIn: '24h' });
-				res.status(200).json({ firstname, lastname, phone, token });
-			})
-			.catch((error) => {
-				console.error(error);
-				res.status(500).json({ error });
-			});
-	} else {
-		res.status(400).json({ error: 'Invalid arguments' });
+	const { firstname, lastname, phone } = req.body;
+
+	if (!firstname || !lastname || !phone || phone != decodedToken.phone) {
+		return res.status(400).json({ error: 'Invalid arguments' });
+	}
+
+	const findQueryResult = await db.query('SELECT phone FROM users WHERE phone = $1', [phone]);
+
+	if (findQueryResult.rowCount != 0) {
+		return res.status(409).json({ message: 'User already exists' });
+	}
+
+	try {
+		const insertQueryString = 'INSERT INTO users (firstname, lastname, phone) VALUES ($1, $2, $3) RETURNING user_id';
+		const insertQueryParams = [firstname, lastname, phone];
+		const insertQueryResult = await db.query(insertQueryString, insertQueryParams);
+		const { user_id } = insertQueryResult.rows[0];
+		const token = jwt.sign({ user_id, firstname, lastname, phone }, jwtSecret, { expiresIn: '24h' });
+		res.status(200).json({ user_id, firstname, lastname, phone, token });
+	} catch {
+		res.status(500).json({ error: 'Unable to sign you up' });
 	}
 };
 
 exports.login = async function (req, res, next) {
-	const token = req.headers.authorization.split(' ')[1];
+	const decodedToken = (function () {
+		try {
+			const token = req.headers.authorization.split(' ')[1];
+			return jwt.decode(token, jwtSecret);
+		} catch {
+			return undefined;
+		}
+	})();
 
-	const decodedToken = jwt.decode(token, jwtSecret);
+	if (!decodedToken) {
+		return res.status(401).json({ error: 'Invalid token' });
+	}
 
 	const phone = decodedToken.phone;
 
 	if (req.body.phone && req.body.phone !== phone) {
-		res.status(400).json({ error: 'Invalid phone number' });
+		return res.status(400).json({ error: 'Invalid phone number' });
+	}
+
+	const { rows } = await db.query('SELECT * FROM users WHERE phone = $1', [phone]);
+
+	if (rows.length <= 0) {
+		res.status(401).json({ error: 'No user found' });
 	} else {
-		const { rows } = await db.query('SELECT * FROM users WHERE phone = $1', [phone]);
-		if (rows.length <= 0) {
-			res.status(401).json({ error: 'No user found' });
-		} else {
-			const { firstname, lastname } = rows[0];
-			const token = jwt.sign({ firstname, lastname, phone }, jwtSecret, { algorithm: 'HS256', expiresIn: '24h' });
-			res.status(200).json({ firstname, lastname, phone, token });
-		}
+		const { user_id, firstname, lastname } = rows[0];
+		const token = jwt.sign({ user_id, firstname, lastname, phone }, jwtSecret, { expiresIn: '24h' });
+		res.status(200).json({ user_id, firstname, lastname, phone, token });
 	}
 };
