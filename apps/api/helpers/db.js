08c0bf97ec57eb14ebbde41735fcdb2f6949f8dc
Alex N'Guessan <alexmarcaureln@gmail.com>
2021-11-09 10:52:43 +0000

    Can use database url in production

--- apps/api/helpers/db.js
+++ apps/api/helpers/db.js
@@ -1,12 +1,15 @@
 const { Pool } = require('pg');
 
-const pool = new Pool({
+const connectionString = process.env.DATABASE_URL;
+const config = {
 	host: process.env.PGHOST,
 	port: process.env.PGPORT,
 	user: process.env.PGUSER,
 	password: process.env.PGPASSWORD,
 	database: process.env.PGDATABASE,
-});
+};
+
+const pool = new Pool(connectionString ? { connectionString } : config);
 
 module.exports = {
 	query: (queryString, params) => pool.query(queryString, params),
