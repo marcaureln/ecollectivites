5e20a3f821ee7898efffd8d7e456a6cf96339d53
Alex N'Guessan <alexmarcaureln@gmail.com>
2022-02-27 08:46:38 +0000

    WIP: add agent messaging capability

--- apps/api/src/controllers/agent.js
+++ apps/api/src/controllers/agent.js
@@ -61,3 +61,86 @@ exports.setPassword = async function (req, res, next) {
 		return res.status(500).send(error.message);
 	}
 };
+
+exports.sendMessage = async function (req, res, next) {
+	const { agent_num, recipient, msgcontent } = req.body;
+
+	try {
+		await db.query('INSERT INTO message (msgcontent, msgsenddate, agent_num, recipient) VALUES ($1, $2, $3, $4)', [
+			msgcontent,
+			new Date(),
+			agent_num,
+			recipient,
+		]);
+		return res.status(200).send();
+	} catch (error) {
+		return res.status(500).send(error.message);
+	}
+};
+
+exports.getPenPals = async function (req, res, next) {
+	const { agent_num } = req.body;
+
+	try {
+		const { rows } = await db.query('SELECT agent_num, recipient FROM message WHERE agent_num = $1 OR recipient = $1', [
+			agent_num,
+		]);
+		let penpals = rows.map((row) => Object.values(row));
+		penpals = [...new Set(penpals.flat())].filter((recipient) => recipient != agent_num);
+		return res.status(200).json(penpals);
+	} catch (error) {
+		return res.status(500).send(error.message);
+	}
+};
+
+exports.getConversation = async function (req, res, next) {
+	const { agent_num, penpal } = req.body;
+
+	try {
+		const { rows } = await db.query(
+			'SELECT * FROM message WHERE agent_num = $1 AND recipient = $2 OR agent_num = $2 AND recipient = $1 ORDER BY msgsenddate',
+			[agent_num, penpal]
+		);
+		return res.status(200).json(rows);
+	} catch (error) {
+		return res.status(500).send(error.message);
+	}
+};
+
+exports.getAgentInfo = async function (req, res, next) {
+	const agent_num = req.params.num;
+
+	try {
+		const { rows } = await db.query(
+			'SELECT agent_num, agentfirstname, agentlastname, agentrole, collect_id FROM agent WHERE agent_num = $1',
+			[agent_num]
+		);
+		if (rows.length < 1) {
+			return res.status(404).send('No agent found');
+		}
+		return res.status(200).json(rows[0]);
+	} catch (error) {
+		return res.status(500).send(error.message);
+	}
+};
+
+exports.searchAgent = async function (req, res, next) {
+	const query = req.query.q;
+
+	if (!query) {
+		return res.status(400).send('Invalid request');
+	}
+
+	try {
+		const { rows } = await db.query(
+			'SELECT agent_num, agentfirstname, agentlastname, agentrole, agent.collect_id, collectname FROM agent, collectivite WHERE agent.collect_id = collectivite.collect_id AND (LOWER(agent_num) = $1 OR LOWER(collectname) LIKE $1 OR LOWER(agentfirstname) LIKE $1 OR LOWER(agentlastname) LIKE $1)',
+			[query.toLowerCase()]
+		);
+		if (rows.length < 1) {
+			return res.status(404).send('No results');
+		}
+		return res.status(200).json(rows);
+	} catch (error) {
+		return res.status(500).send(error.message);
+	}
+};
--- apps/api/src/routes/agent.js
+++ apps/api/src/routes/agent.js
@@ -5,5 +5,12 @@ const agentController = require('../controllers/agent');
 
 router.post('/auth/login', agentController.login);
 router.post('/auth/set-password', agentController.setPassword);
+router.post('/messages', agentController.sendMessage);
+router.post('/penpals', agentController.getPenPals);
+router.post('/conversations', agentController.getConversation);
+// router.get('/contacts', agentController.getCollectivitesAgents);
+router.get('/contacts/search', agentController.searchAgent);
+router.get('/contacts/:num', agentController.getAgentInfo);
+// router.get('/inbox', agentController.getCollectivitesRequests);
 
 module.exports = router;
