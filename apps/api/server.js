27684e093ac2eb3f441d5548d59810df0229b232
Alex N'Guessan <alexmarcaureln@gmail.com>
2022-03-03 17:24:00 +0000

    Add 🚀 to app starting console log

--- apps/api/server.js
+++ apps/api/server.js
@@ -3,5 +3,5 @@ const app = require('./src/app');
 const port = process.env.PORT || 3000;
 
 const listener = app.listen(port, () => {
-	console.log('Your app is listening on port ' + listener.address().port);
+	console.log('🚀 Your app is listening on port ' + listener.address().port);
 });
