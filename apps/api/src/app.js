213449a651e0b9e3ddd80b9a47b48ad0305ba8e4
Alex N'Guessan <alexmarcaureln@gmail.com>
2022-03-19 16:57:34 +0000

    Use a single file for API routes registration

--- apps/api/src/app.js
+++ apps/api/src/app.js
@@ -7,23 +7,11 @@ const { errorLogger, errorHandler, routeNotFound } = require('./helpers/error');
 
 const app = express();
 
-const authRoutes = require('./routes/auth');
-const collectiviteRoutes = require('./routes/collectivite');
-const requestRoutes = require('./routes/request');
-const responseRoutes = require('./routes/response');
-const userRoutes = require('./routes/user');
-const myRoutes = require('./routes/me');
-
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/static', express.static(path.join(__dirname, '../public')));
-app.use('/api/auth', authRoutes);
-app.use('/api/collectivites', collectiviteRoutes);
-app.use('/api/requests', requestRoutes);
-app.use('/api/responses', responseRoutes);
-app.use('/api/users', userRoutes);
-app.use('/api/me', myRoutes);
+app.use('/api', require('./routes/api'));
 app.use(routeNotFound);
 app.use(errorLogger);
 app.use(errorHandler);
