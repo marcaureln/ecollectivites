f376a0e27085c16f7bbc57011e4f7f401c4ec2df
Alex N'Guessan <alexmarcaureln@gmail.com>
2022-05-14 19:04:04 +0000

    Add missing import

--- apps/api/src/controllers/request.js
+++ apps/api/src/controllers/request.js
@@ -1,5 +1,5 @@
 const { PrismaClient } = require('@prisma/client');
-const { ac } = require('../helpers/accesscontrol');
+const { ac, isSameCollectivite } = require('../helpers/accesscontrol');
 const { AppError } = require('../helpers/error');
 
 /** @type { PrismaClient } **/
