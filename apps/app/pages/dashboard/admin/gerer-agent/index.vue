ed76f9620a058d8f512eee37d81bacc617e71436
Alex N'Guessan <alexmarcaureln@gmail.com>
2022-05-17 15:47:31 +0000

    WIP: manage agents

--- apps/app/pages/dashboard/admin/gerer-agent/index.vue
+++ apps/app/pages/dashboard/admin/gerer-agent/index.vue
@@ -1,5 +1,38 @@
 <template>
-  <div class=""></div>
+  <div class="manage-agent--wrapper">
+    <h1>Gérer les agents</h1>
+
+    <table>
+      <thead>
+        <tr>
+          <td>ID</td>
+          <td>Nom</td>
+          <td>Prénoms</td>
+          <td>Adresse mail</td>
+          <td>Téléphone</td>
+          <td>Rôle</td>
+          <td>Actions</td>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="user in users" :key="user.userId">
+          <td>{{ user.userId }}</td>
+          <td>{{ user.lastname }}</td>
+          <td>{{ user.firstname }}</td>
+          <td>
+            <a :href="'mailto:' + user.email">{{ user.email }}</a>
+          </td>
+          <td>
+            <a :href="'tel:' + user.phone">{{ user.phone }}</a>
+          </td>
+          <td>{{ user.role }}</td>
+          <td>
+            <nuxt-link :to="'/dashboard/admin/gerer-agent/' + user.userId">Voir l'agent</nuxt-link>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script>
