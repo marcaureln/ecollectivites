3ddce043dc29da5ef461594988b0e8b4af77c5a0
Alex N'Guessan <alexmarcaureln@gmail.com>
2022-11-01 22:14:22 +0000

    Add compose file for the whole monorepo and a gh actions workflow

--- apps/app/nuxt.config.js
+++ apps/app/nuxt.config.js
@@ -3,29 +3,29 @@ export default {
   telemetry: false,
 
   // Target: https://go.nuxtjs.dev/config-target
-  target: "static",
+  target: 'static',
 
   // No server-side rendering (only client-side navigation)
   ssr: false,
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: "ecollectivites",
+    title: 'ecollectivites',
     htmlAttrs: {
-      lang: "fr",
+      lang: 'fr',
     },
     meta: [
-      { charset: "utf-8" },
-      { name: "viewport", content: "width=device-width, initial-scale=1" },
-      { hid: "description", name: "description", content: "" },
-      { name: "format-detection", content: "telephone=no" },
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' },
     ],
-    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
-    script: [{ src: "https://unpkg.com/ionicons@4.5.10-0/dist/ionicons.js", body: true }],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    script: [{ src: 'https://unpkg.com/ionicons@4.5.10-0/dist/ionicons.js', body: true }],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: ["@/assets/css/main.scss"],
+  css: ['@/assets/css/main.scss'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
@@ -39,27 +39,27 @@ export default {
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     // https://go.nuxtjs.dev/axios
-    "@nuxtjs/axios",
+    '@nuxtjs/axios',
     // https://google-fonts.nuxtjs.org/
-    "@nuxtjs/google-fonts",
+    '@nuxtjs/google-fonts',
     // https://github.com/nuxt-community/style-resources-module
-    "@nuxtjs/style-resources",
+    '@nuxtjs/style-resources',
     // https://github.com/nuxt-community/eslint-module
-    "@nuxtjs/eslint-module",
+    '@nuxtjs/eslint-module',
     // https://github.com/nuxt-community/community-modules/tree/master/packages/toast
-    "@nuxtjs/toast",
+    '@nuxtjs/toast',
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: process.env.API_BASE_URL || "/api",
+    baseURL: process.env.API_BASE_URL || '/api',
     headers: {
       common: {
-        Accept: "application/json, text/plain, */*",
+        Accept: 'application/json, text/plain, */*',
       },
       post: {
-        "Content-Type": "application/json; charset=utf-8;",
+        'Content-Type': 'application/json; charset=utf-8;',
       },
     },
   },
@@ -67,27 +67,31 @@ export default {
   googleFonts: {
     families: {
       Roboto: [300, 400, 700],
-      "Barlow+Condensed": [700],
+      'Barlow+Condensed': [700],
     },
     download: true,
     inject: true,
   },
 
   styleResources: {
-    scss: ["./assets/css/variables.scss", "./assets/css/mixins.scss"],
+    scss: ['./assets/css/variables.scss', './assets/css/mixins.scss'],
   },
 
   // Toast plugin configuration: https://github.com/shakee93/vue-toasted
   toast: {
-    position: "bottom-center",
+    position: 'bottom-center',
     duration: 1000,
     keepOnHove: true,
   },
 
   router: {
-    middleware: ["auth"],
+    middleware: ['auth'],
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
+
+  server: {
+    host: '0.0.0.0',
+  },
 };
